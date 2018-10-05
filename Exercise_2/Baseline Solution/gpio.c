a4b79ab011d1061a0760f3149315141eefa46d1b
klovjan <eivind.klo@gmail.com>
2018-10-05 11:56:14 +0200

    Set buttons and LEDs as I/O (2)

--- Exercise_2/Baseline Solution/gpio.c
+++ Exercise_2/Baseline Solution/gpio.c
@@ -9,16 +9,19 @@
 void setupGPIO()
 {
 	/*
-	 * TODO set input and output pins for the joystick 
-	 */
-
-	/*
 	 * Example of HW access from C code: turn on joystick LEDs D4-D8 check 
 	 * efm32gg.h for other useful register definitions 
 	 */
 	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	/* enable GPIO clock */
+	
+	/*
+	 * Set up gamepad LED lights as output 
+	 */
 	*GPIO_PA_CTRL = 2;	/* set high drive strength */
 	*GPIO_PA_MODEH = 0x55555555;	/* set pins A8-15 as output */
-	*GPIO_PA_DOUT = 0x0700;	/* turn on LEDs D4-D8 (LEDs are active
-				 * low) */
+	
+	/*
+	 * Set up gamepad buttons as input
+	 */
+	*GPIO_PC_MODEL = 0x33333333;	/* set pins D0-7 as input */
 }
