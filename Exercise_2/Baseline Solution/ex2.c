af732c2066f41ea3a37b83fe57d6e53b0171b33b
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2018-10-28 16:44:56 +0100

    created one common.h header, cleaned up some code

--- Exercise_2/Baseline Solution/ex2.c
+++ Exercise_2/Baseline Solution/ex2.c
@@ -1,35 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
-#include <math.h>
 #include "efm32gg.h"
-#include "math.h"
-#include "songs.c"
-#include "tones.h"
-
-/*
- * The period between sound samples, in clock cycles 
- * 317 as sample period results in a sample rate of approximately 44100Hz
- */
-#define   SAMPLE_PERIOD 317
-#define CHECK_BTN(input,btn) (~(input) & (1<<(btn)))		//Used to see if a button is pressed (active low)
-
-/*
-*	Songs
-*/
-int happyBirthday[] = { C4, R, C4, D4, D4, D4, C4, C4, C4, F4, F4, F4, E4, E4, E4, E4, E4, R, C4, R, C4, D4, D4, D4, C4, C4, C4, G4, G4, G4, F4, F4, F4, F4, F4, R, C4, R, C4, C5, C5, C5, A4, A4, A4, F4, R, F4, E4, E4, E4, D4, D4, D4, B4, R, B4, A4, A4, A4, F4, F4, F4, G4, G4, G4, F4, F4, F4, F4 };
-
-int acidSong[] = {C6, C6, R, A6, A6, R, A6, A6, R, C6, C6, E5};
-int lostLife[] = {E4, F4, Fs4, G4, A4, G4, F4, Ds4};
-int temp[] = {C5, C5, C5, C5, C5, G4, G4, G4, G4, E4, E4, E4, E4, C4, C4, C4, C4, C4 };
-
-/*
- * Declaration of peripheral setup functions 
- */
-void setupTimer(uint32_t period);
-void setupGPIO();
-void setupDAC();
-int readButtons();
-void handleInput();
+#include "common.h"
 
 
 /*
@@ -60,15 +32,15 @@ int main(void)
 void handleInput(int buttonValues)
 {
 	if(CHECK_BTN(buttonValues, 5)){ //If button 6 is pressed
-		playSong(happyBirthday, sizeof(happyBirthday)/sizeof(int), 2500);
+		playSong(explosion, sizeof(explosion)/sizeof(int), 2500);
 	}
 	else if(CHECK_BTN(buttonValues, 6)){
-		playSong(acidSong, sizeof(acidSong)/sizeof(int), 1000);
+		playSong(happyBirthday, sizeof(happyBirthday)/sizeof(int), 1000);
 	}
 	else if(CHECK_BTN(buttonValues, 7)){
-		playSong(lostLife, sizeof(lostLife)/sizeof(int), 1000);
+		playSong(acidSong, sizeof(acidSong)/sizeof(int), 1000);
 	}
 	else if(CHECK_BTN(buttonValues, 4)){
-		playSong(temp, sizeof(temp)/sizeof(int), 1000);
+		playSong(acidSound, sizeof(acidSound)/sizeof(int), 1000);
 	}
 }
--- Exercise_2/Baseline Solution/songs.c
+++ Exercise_2/Baseline Solution/songs.c
@@ -1,6 +1,5 @@
 #include "efm32gg.h"
-
-void playNote(int frequency, int noteLength);
+#include "common.h"
 
 void playSong(int* notes, int noteCount, int noteLength){	
 	
@@ -43,3 +42,14 @@ void playNote(int frequency, int noteLength){
 		clockVal = *TIMER1_CNT;						//Continuously read the timer count
 	}
 }
+
+/*
+ *	Songs
+ */
+const int happyBirthday[] = { C4, R, C4, D4, D4, D4, C4, C4, C4, F4, F4, F4, E4, E4, E4, E4, E4, R, C4, R, C4, D4, D4, D4, C4, C4, C4, G4, G4, G4, F4, F4, F4, F4, F4, R, C4, R, C4, C5, C5, C5, A4, A4, A4, F4, R, F4, E4, E4, E4, D4, D4, D4, B4, R, B4, A4, A4, A4, F4, F4, F4, G4, G4, G4, F4, F4, F4, F4 };
+
+const int acidSong[] = {C4, R, C4, D4, D4,C4, R, C4, D4, D4,C4, R, C4, D4, D4,C4, R, C4, D4, D4};
+
+const int acidSound[] = {G4, B4, G5, B5, G6, G6, Fs5, C5,G4, B4, G5, B5, G6, G6, Fs5, C5,G4, B4, G5, B5, G6, G6, Fs5, C5};
+
+const int explosion[] = {G4/12, B4/12, G5/12, B5/12, G6/12, G6/12, Fs5/12, C5/12,G4/12, B4/6, G5/6, B5/6, G6/4, G6/4, Fs5/4, C5/4,G4/4, B4/4, G5/4, B5/4, G6/4, G6*7, Fs5/4, C5/4,G4*2, B4*4, G5/12, B5/12, G6/12, G6/12, Fs5/12, C5/12,G4/12, B4/6, G5/6, B5/6, G6/4, G6/4, Fs5/4, C5/4,G4/4, B4/4, G5/4, B5*5, G6/4, G6/4, Fs5/4, C5/4};
