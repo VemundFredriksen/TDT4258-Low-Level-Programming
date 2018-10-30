b769640954eda20a69eeaf6e614e6d422fbc9487
klovjan <eivind.klo@gmail.com>
2018-10-30 16:53:51 +0100

    Increase tempo of sounds in Baseline to roughly match Improved

--- Exercise_2/Baseline Solution/songs.c
+++ Exercise_2/Baseline Solution/songs.c
@@ -61,22 +61,22 @@ float happyBeats[] =
     1,1,1, 		3,		3, 			1,1,1,
     3,			3,		1,1,1,		3,
     3,			3,		4};
-int happyBPM = 1058;
+int happyBPM = 1500;
 int* happySamples;
 
 int acid1[] = {C4, R, C4, D4, D4, C4, R, C4, D4, D4, C4, R, C4, D4, D4, C4, R, C4, D4, D4};
 float acid1Beats[] = {[0 ... 19] = 1};
-int acid1BPM = 2646;
+int acid1BPM = 6615;
 int* acid1Samples;
 
 int acid2[] = {G4, B4, G5, B5, G6, G6, Fs5, C5,G4, B4, G5, B5, G6, G6, Fs5, C5,G4, B4, G5, B5, G6, G6, Fs5, C5};
 float acid2Beats[] = {[0 ... 23] = 1};
-int acid2BPM = 2646;
+int acid2BPM = 6615;
 int* acid2Samples;
 
 int explosion[] = {G4/12, B4/12, G5/12, B5/12, G6/12, G6/12, Fs5/12, C5/12,G4/12, B4/6, G5/6, B5/6, G6/4, G6/4, Fs5/4, C5/4,G4/4, B4/4, G5/4, B5/4, G6/4, G6*7, Fs5/4, C5/4,G4*2, B4*4, G5/12, B5/12, G6/12, G6/12, Fs5/12, C5/12,G4/12, B4/6, G5/6, B5/6, G6/4, G6/4, Fs5/4, C5/4,G4/4, B4/4, G5/4, B5*5, G6/4, G6/4, Fs5/4, C5/4};
 float explosionBeats[] = {[0 ... 47] = 1};
-int explosionBPM = 4000;
+int explosionBPM = 10000;
 int* explosionSamples;
 
 int *instantiateSong(int songIndex)
