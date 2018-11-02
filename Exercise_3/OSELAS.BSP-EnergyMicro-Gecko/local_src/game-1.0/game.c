3e602b9d353838e3393e575c605da7011cd6c62e
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2018-11-02 15:37:05 +0100

    installed linux and everything is running!

--- Exercise_3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ Exercise_3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
 
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
-
+	
+	int fbfd = open("/dev/fb0", 2);
+	
+	unsigned short* screen;
+	screen = (unsigned short*) mmap(NULL, 2*320*240, PROT_WRITE | PROT_READ, MAP_SHARED, fbfd, 0);
+	int i;
+	
+	for(i = 0; i < 2*38400; i++){
+		screen[i] = 0x001F;
+	}
+	
+	struct fb_copyarea rect;
+	
+	rect.dx = 0;
+	rect.dy = 0;
+	rect.width = 320;
+	rect.height = 240;
+	
+	
+	ioctl(fbfd, 0x4680, &rect);
 	exit(EXIT_SUCCESS);
 }
